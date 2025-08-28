1e5b4f169fafe2d4aa9e6e582a9c068d2e932af9
Alex Esson <Ghostshadow1222@users.noreply.github.com>
2025-08-27 17:20:17 -0700

    Add ProductDBContext for managing Product entities
    Introduce a new `ProductDBContext` class inheriting from `DbContext` to manage the database context for `Product` entities. The class includes a constructor for `DbContextOptions<ProductDBContext>` and a `DbSet<Product>` property for tracking `Product` entities. Necessary namespaces are also included.

---
+++ eCommercePractice/Data/ProductDBContext.cs
@@ -0,0 +1,15 @@
+﻿using eCommercePractice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommercePractice.Data;
+
+public class ProductDBContext : DbContext
+{
+    public ProductDBContext(DbContextOptions<ProductDBContext> options) : base(options)
+    {
+
+    }
+
+    // Entites to be tracked by DbContext
+    public DbSet<Product> Products { get; set; }
+}
