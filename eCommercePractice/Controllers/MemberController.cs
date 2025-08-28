b03602d48264399b6e0ec02e7ae49baeb0d78193
Alex Esson <Ghostshadow1222@users.noreply.github.com>
2025-08-28 16:30:30 -0700

    Add member registration functionality in MemberController
    - Introduced new using directives for data and model namespaces.
    - Added a private ProductDBContext field for database operations.
    - Implemented a constructor to initialize the database context.
    - Created a Register action method to handle GET and POST requests.
    - Validates the registration form, maps data to a Member model, and saves it to the database.
    - Redirects to the home page upon successful registration or returns the view with errors.

--- eCommercePractice/Controllers/MemberController.cs
+++ eCommercePractice/Controllers/MemberController.cs
@@ -1,11 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommercePractice.Data;
+using eCommercePractice.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eCommercePractice.Controllers;
 
 public class MemberController : Controller
 {
+    private readonly ProductDBContext _context;
+
+    public MemberController(ProductDBContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Register()
     {
         return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegistrationViewModel reg)
+    {
+        if (ModelState.IsValid)
+        {
+            // Map ViewModel to Member model tracked by DB
+            Member newMember = new()
+            {
+                Username = reg.Username,
+                Email = reg.Email,
+                Password = reg.Password,
+                DateOfBirth = reg.DateOfBirth,
+            };
+
+            _context.Members.Add(newMember);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        return View(reg);
     }
 }
