1f9b1ed2fbb3e35014bc0492ab5b722b2a2ff3f6
Alex Esson <Ghostshadow1222@users.noreply.github.com>
2025-08-27 17:41:42 -0700

    Add DB context configuration and necessary using directives
    This commit introduces using directives for `eCommercePractice.Data` and `Microsoft.EntityFrameworkCore`. It configures the `ProductDBContext` to use SQL Server with the "DefaultConnection" string, enabling Entity Framework Core for database operations. Additionally, it initializes the web application builder and builds the application instance, following standard ASP.NET Core setup practices.

--- eCommercePractice/Program.cs
+++ eCommercePractice/Program.cs
@@ -1,7 +1,15 @@
+using eCommercePractice.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configure DB Context settings
+builder.Services.AddDbContext<ProductDBContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
