5c3c4e895f69e64d019c88482fb595779e779c42
Alex Esson <Ghostshadow1222@users.noreply.github.com>
2025-08-28 15:33:41 -0700

    Refactor Member class into RegistrationViewModel
    The `Member` class has been refactored, moving its properties to a new class called `RegistrationViewModel`. This new class includes `Username`, `Email`, `Password`, `ConfirmPassword`, and `DateOfBirth` with appropriate validation attributes. The original `Member` class no longer contains these properties, indicating a separation of concerns for handling registration logic.

--- eCommercePractice/Models/Member.cs
+++ eCommercePractice/Models/Member.cs
@@ -19,13 +19,11 @@ public class Member
     /// </summary>
     [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "Username can only contain alphanumeric characters (letters and numbers only).")]
     [StringLength(25, ErrorMessage = "Your username cannot be more than 25 characters")]
-
     public required string Username { get; set; }
 
     /// <summary>
     /// Email address of the member
     /// </summary>
-
     public required string Email { get; set; }
 
     /// <summary>
@@ -38,6 +36,38 @@ public class Member
     /// The date when the member was born
     /// </summary>
     public DateOnly DateOfBirth { get; set; }
+}
 
+public class RegistrationViewModel
+{
+    /// <summary>
+    /// Public facing username defined by the member
+    /// Alphanumeric characters only
+    /// </summary>
+    [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "Username can only contain alphanumeric characters (letters and numbers only).")]
+    [StringLength(25, ErrorMessage = "Your username cannot be more than 25 characters")]
+    public required string Username { get; set; }
 
+    /// <summary>
+    /// Email address of the member
+    /// </summary>
+    [DataType(DataType.EmailAddress)]
+    public required string Email { get; set; }
+
+    /// <summary>
+    /// Member's password
+    /// </summary>
+    [StringLength(50, MinimumLength = 6, ErrorMessage = "Your password must be between 6 and 50 characters")]
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+
+    [Compare(nameof(Password))]
+    [DataType(DataType.Password)]
+    public required string ConfirmPassword { get; set; }
+
+    /// <summary>
+    /// The date when the member was born
+    /// </summary>
+    [DataType(DataType.Date)]
+    public DateOnly DateOfBirth { get; set; }
 }
